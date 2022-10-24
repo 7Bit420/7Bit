cd81aaa114fefff0640456f0c709bcad39708105
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-10-24 21:59:22 +0000

    Update

---
+++ primeFind.py
@@ -0,0 +1,18 @@
+primes = list()
+maxPrime = 10_000_000
+
+
+def isPrime(p):
+    l = True
+    for n in range(2, p - 1):
+        if (p % n == 0):
+            l = False
+            break
+    return l
+
+
+for i in range(2, maxPrime):
+    if isPrime(i):
+        primes.append(i)
+
+print(primes)
